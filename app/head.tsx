04b5758dfd2fac693f2893f4b4b2b88b1e834778
Bhupattii <92458357+Bhupattii@users.noreply.github.com>
2023-05-22 21:49:15 +0530

    Add files via upload

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Bhupathi Raju</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="Portfolio" content="This is my Portfolio Site" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
